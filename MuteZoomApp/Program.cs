8f082a73be53d8d3659d44d676fd4b7abf50f497
forestb <forest.balk@gmail.com>
2021-07-07 08:53:36 -0600

    Initial commit

---
+++ MuteZoomApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MuteZoomApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
